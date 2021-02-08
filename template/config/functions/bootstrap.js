75821f07e51ce38da03ad4466b91e93b78ed1771
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-08 11:23:35 +0100

    first commit

---
+++ template/config/functions/bootstrap.js
@@ -0,0 +1,148 @@
+"use strict";
+const { products, categories, global } = require("../../data/data.json");
+const fs = require("fs");
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/developer-docs/latest/concepts/configurations.html#bootstrap
+ */
+
+async function isFirstRun() {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup",
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+}
+
+async function setPublicPermissions(newPermissions) {
+  // Find the ID of the public role
+  const publicRole = await strapi
+    .query("role", "users-permissions")
+    .findOne({ type: "public" });
+
+  // List all available permissions
+  const publicPermissions = await strapi
+    .query("permission", "users-permissions")
+    .find({ type: "application", role: publicRole.id });
+
+  // Update permission to match new config
+  const controllersToUpdate = Object.keys(newPermissions);
+  const updatePromises = publicPermissions
+    .filter((permission) => {
+      // Only update permissions included in newConfig
+      if (!controllersToUpdate.includes(permission.controller)) {
+        return false;
+      }
+      if (!newPermissions[permission.controller].includes(permission.action)) {
+        return false;
+      }
+      return true;
+    })
+    .map((permission) => {
+      // Enable the selected permissions
+      return strapi
+        .query("permission", "users-permissions")
+        .update({ id: permission.id }, { enabled: true });
+    });
+
+  await Promise.all(updatePromises);
+}
+
+function getFileSizeInBytes(filePath) {
+  const stats = fs.statSync(filePath);
+  const fileSizeInBytes = stats["size"];
+  return fileSizeInBytes;
+}
+
+function getFileData(fileName) {
+  const filePath = `./data/uploads/${fileName}`;
+
+  // Parse the file metadata
+  const size = getFileSizeInBytes(filePath);
+  const ext = fileName.split(".").pop();
+  const mimeType = `image/${ext === "svg" ? "svg+xml" : ext}`;
+
+  return {
+    path: filePath,
+    name: fileName,
+    size,
+    type: mimeType,
+  };
+}
+
+// Create an entry and attach files if there are any
+async function createEntry(model, entry, files) {
+  try {
+    const createdEntry = await strapi.query(model).create(entry);
+    if (files) {
+      await strapi.entityService.uploadFiles(createdEntry, files, {
+        model,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function importGlobal() {
+  const files = {
+    "favicon": getFileData("favicon.png"),
+    "defaultSeo.shareImage": getFileData("placeholder.png"),
+    "placeHolder": getFileData("placeholder.png")
+  };
+
+  return createEntry("global", global, files);
+}
+
+
+async function importCategories() {
+  categories.forEach(async (category) => {
+    const files = {
+      image: getFileData(`${category.slug}.png`)
+    }
+    await createEntry("category", category, files);
+  });
+}
+
+async function importProducts() {
+  products.forEach(async (product) => {
+    const files = {
+      image: getFileData(`${product.slug}.png`)
+    }
+    await createEntry("product", product, files);
+  });
+}
+
+async function importSeedData() {
+  // Allow read of application content types
+  await setPublicPermissions({
+    global: ["find"],
+    product: ["find", "findone"],
+    category: ["find", "findone"],
+  });
+
+  // Create all entries
+  await importGlobal()
+  await importProducts();
+  await importCategories();
+}
+
+module.exports = async () => {
+  const shouldImportSeedData = await isFirstRun();
+  if (shouldImportSeedData) {
+    try {
+      await importSeedData();
+    } catch (error) {
+      console.log("Could not import seed data");
+      console.error(error);
+    }
+  }
+};
